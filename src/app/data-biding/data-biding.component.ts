1eac5bbbfb71ca5889586e9fc357771a85794a29
Digo123 <rodrigo.arendt@hotmail.com>
2023-01-09 08:13:29 -0300

    "Aprendendo sobre Property Binding"

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -10,8 +10,12 @@ export class DataBidingComponent implements OnInit {
   public nome: string = "Rodrigo";
   public idade: number = 36;
 
+  public checkedDisabled: boolean = false;
+  public imgSrc: string ="https://mundoconectado.com.br/uploads/chamadas/ia-consciencia-chamada.jpg";
+  public imgTitle: string = "Property Binding"
   constructor() { }
-ngOnInit(): void {
 
-}
+  ngOnInit(): void {
+
+  }
 }
