be57da527060046895e0b7a3bee38cd5b18daee3
Digo123 <rodrigo.arendt@hotmail.com>
2023-01-09 10:07:18 -0300

    "Aprendendo sobre Event Binding"

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -11,11 +11,20 @@ export class DataBidingComponent implements OnInit {
   public idade: number = 36;
 
   public checkedDisabled: boolean = false;
-  public imgSrc: string ="https://mundoconectado.com.br/uploads/chamadas/ia-consciencia-chamada.jpg";
+  public imgSrc: string = "https://mundoconectado.com.br/uploads/chamadas/ia-consciencia-chamada.jpg";
   public imgTitle: string = "Property Binding"
+
+  public position: { x: number, y: number } = { x: 0, y: 0 };
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
 
+  public alertaInfo(valor: MouseEvent) {
+    console.log(valor);
+  }
+
+  public mouseMoveTest(valor: MouseEvent) {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetX;
   }
 }
