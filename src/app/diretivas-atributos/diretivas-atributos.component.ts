5979fb418109d3460f5d90d2b6bdb585b871f50a
Digo123 <rodrigo.arendt@hotmail.com>
2023-01-16 13:28:13 -0300

    "Aula sobre ngModel"

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -10,24 +10,18 @@ export class DiretivasAtributosComponent implements OnInit {
   public valor: boolean = true;
   public heigthPx: string = "20px";
   public backgroundColor: string = 'red';
+  public nome: string = "";
+  public list: Array<{nome:string}> = [];
+
   constructor() { }
 
   ngOnInit(): void {
 
-    setInterval( ()=> {
-      if(this.valor){
-        this.valor = false;
-      }else{
-        this.valor = true;
-      }
+  }
 
-      if (this.heigthPx == "20px") {
-        this.heigthPx = "50px";
-      } else {
-        this.heigthPx = "20px";
-      }
-    },2000);
-
+  public salvar(){
+    this.list.push({nome: this.nome});
+    this.nome = "";
   }
 
 }
